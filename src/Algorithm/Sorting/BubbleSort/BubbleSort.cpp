ad7d5d9484dcf388827d91568905bf7419da3a5e
vicky <vickyrules1705@gmail.com>
2022-07-20 15:09:10 +0530

    comments added

--- src/Algorithm/Sorting/BubbleSort/BubbleSort.cpp
+++ src/Algorithm/Sorting/BubbleSort/BubbleSort.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 using namespace std;
 
+// perform bubble sort
 void bubbleSort(int array[], int size) {
 
   // loop to access each array element
@@ -11,10 +12,12 @@ void bubbleSort(int array[], int size) {
     // loop to compare array elements
     for (int i = 0; i < size - step; ++i) {
 
-      
-      
+      // compare two adjacent elements
+      // change > to < to sort in descending order
       if (array[i] > array[i + 1]) {
 
+        // swapping elements if elements
+        // are not in the intended order
         int temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
@@ -23,7 +26,7 @@ void bubbleSort(int array[], int size) {
   }
 }
 
-
+// print array
 void printArray(int array[], int size) {
   for (int i = 0; i < size; ++i) {
     cout << "  " << array[i];
@@ -34,7 +37,7 @@ void printArray(int array[], int size) {
 int main() {
   int data[] = {-2, 45, 0, 11, -9};
   
- 
+  // find array's length
   int size = sizeof(data) / sizeof(data[0]);
   
   bubbleSort(data, size);
