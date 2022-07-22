1a47fe15fab7a2fa80aaf7ef86b26916a52d8570
Rakesh Roshan <roshanrakesh7362@gmail.com>
2022-07-22 21:24:23 +0530

    Adding missing Array Leetcode solutions in Java and C++

--- src/Algorithm/Arrays/JumpGame/Jump_Game_II.java
+++ src/Algorithm/Arrays/JumpGame/Jump_Game_II.java
@@ -1,21 +1,3 @@
-/**
- * <br> Problem Statement :
- *
- * Given an DSA.array of non-negative integers nums, you are initially positioned at the first index of the DSA.array.
- * Each element in the DSA.array represents your maximum jump length at that position.
- * Your goal is to reach the last index in the minimum number of jumps.
- * You can assume that you can always reach the last index.
- *
- * Example 1 :
- *
- * Input: nums = [2,3,1,1,4]
- * Output: 2
- * Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
- *
- * </br>
- *
- */
-
 package Algorithm.Arrays.JumpGame;
 
 public class Jump_Game_II {
---
+++ src/Algorithm/Arrays/JumpGame/Readme.md
@@ -0,0 +1,1 @@
+[Problem Link](https://leetcode.com/problems/jump-game-ii/) 
