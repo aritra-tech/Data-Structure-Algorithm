1fe64fc29f17471eeb2b9df2b4a6c097b0b74e38
Avik Mukherjee <77090462+Avik-creator@users.noreply.github.com>
2022-07-23 19:50:49 +0530

    Add files via upload

---
+++ src/Data_Structure/ArrayLists/Problems/ArrayListShuffle.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class ArraylistShuffle {
+    public static void main(String args[])
+    {
+        // creating ArrayList
+        ArrayList<String> al = new ArrayList<String>();
+  
+        // adding object in ArrayList
+        al.add("C");
+        al.add("C++");
+        al.add("Java");
+        al.add("Python");
+        al.add("PHP");
+        al.add("Javascript");
+  
+        System.out.println("Before shuffling Arraylist:");
+  
+        // getting Iterator
+        // from arraylist to
+        // traverse elements
+        Iterator itr = al.iterator();
+  
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+  
+        System.out.println("");
+  
+        Collections.shuffle(al);
+  
+        System.out.println("After shuffling Arraylist:");
+  
+        itr = al.iterator();
+  
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+    }
+}
