cf8e0102bff2ad9d28e8a03e5a13018557e5e8bb
Rakesh Roshan <roshanrakesh7362@gmail.com>
2022-07-23 11:04:54 +0530

    Adding missing Array Leetcode solutions in Java and C++

---
+++ src/Algorithm/Arrays/LeetcodeProblems/TrappingRainWater/TrappingRainWater.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+
+        for(int i = 0; i < height.size(); i++){
+            int maxLeft = 0, maxRight = 0;
+            for(int j = i; j >= 0 ; j--){
+                maxLeft = max(maxLeft, height[j]);
+            }
+            for(int j = i; j < height.size() ; j++){
+                maxRight = max(maxRight, height[j]);
+            }
+            ans += min(maxLeft, maxRight) - height[i];
+            // cout << maxLeft << " " << maxRight << " " << ans << endl;
+        }
+
+        return ans;
+    }
+};
