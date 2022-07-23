f607bbc92ae3ab0392aba78d2f5edf111d39b6a2
Aritra Das <aritrarick2002@gmail.com>
2022-07-23 14:42:21 +0530

    Some changes made in Validate Binary Search Tree code.

--- src/Data_Structure/Tree/Leetcode_Problems/Validate_BST/validate_bst.java
+++ src/Data_Structure/Tree/Leetcode_Problems/Validate_BST/validate_bst.java
@@ -1,3 +1,5 @@
+package Data_Structure.Tree.Leetcode_Problems.Validate_BST;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,18 +15,32 @@
  *     }
  * }
  */
-class Solution {
-    TreeNode prev = null;
-    public boolean isValidBST(TreeNode root) {
-        if(root == null) {
-            return true;
-        }
+public class validate_bst {
 
-        if(isValidBST(root.left) && (prev == null || root.val > prev.val)) {
-            prev = root;
-            return isValidBST(root.right);
-        }
+    static class TreeNode {
+        int val;
+        validate_bst.TreeNode left;
+        validate_bst.TreeNode right;
 
-        return false;
+        TreeNode(int data) {
+            this.val = data;
+            this.left = this.right = null;
+        }
+    }
+    public static void main(String args[])
+    {
+        validate_bst.TreeNode root = new validate_bst.TreeNode(2);
+        root.left = new validate_bst.TreeNode(1);
+        root.right = new validate_bst.TreeNode(3);
+        System.out.println(isValidBST(root));
+    }
+    public static boolean isValidBST(TreeNode root) {
+        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    private static boolean valid(TreeNode root, long min_range, long max_range) {
+        if(root==null) return true;
+        if(root.val >= max_range || root.val <=min_range) return false;
+        return valid(root.left,min_range,root.val) && valid(root.right, root.val , max_range);
     }
 }
