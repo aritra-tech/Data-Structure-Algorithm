29528424a38a0df01d8c4c1c952e7a754084407c
Aritra Das <aritrarick2002@gmail.com>
2022-07-26 13:55:51 +0530

    Some changes made.

---
+++ src/Algorithm/Arrays/LeetcodeProblems/NumberofGoodPairs/README.md
@@ -0,0 +1,2 @@
+## Question link:
+[Number of Good Pairs](https://leetcode.com/problems/number-of-good-pairs/)
--- src/Algorithm/Arrays/LeetcodeProblems/NumberofGoodPairs/goodpairs.java
+++ src/Algorithm/Arrays/LeetcodeProblems/NumberofGoodPairs/goodpairs.java
@@ -1,4 +1,6 @@
-public class Main
+package Algorithm.Arrays.LeetcodeProblems.NumberofGoodPairs;
+
+public class goodpairs
 {
 	public static void main(String[] args) {
 	    int[] nums={1,2,3,1,1,3};
