91884af0a3bb32df94171199e9ea4346c388afed
Rishika Vishnoi <Rishikavishnoi56@gmail.com>
2022-07-28 17:52:58 +0530

    reverse linkedlist in java

---
+++ src/Data_Structure/Linked_List/ReverseLinkedList/ReverseLinkedList.java
@@ -0,0 +1,14 @@
+ package Data_Structure.Linked_List.ReverLinkedList;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
