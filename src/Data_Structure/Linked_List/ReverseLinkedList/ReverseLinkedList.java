d6e69952e718c966b6acf1d6abddd42d1f21e470
Rishika Vishnoi <Rishikavishnoi56@gmail.com>
2022-07-28 17:58:21 +0530

    middleelement solution added in linkedlist

--- src/Data_Structure/Linked_List/ReverseLinkedList/ReverseLinkedList.java
+++ src/Data_Structure/Linked_List/ReverseLinkedList/ReverseLinkedList.java
@@ -1,5 +1,18 @@
  package Data_Structure.Linked_List.ReverLinkedList;
 
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode newHead = null;
