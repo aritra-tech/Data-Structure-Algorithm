f12a7fbc62a6247518083f321c285976fd7d7d25
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2022-08-05 14:13:57 +0530

    Update totalHammingDistance.cpp

--- src/Data_Structure/BitManipulation/totalHammingDistance.cpp
+++ src/Data_Structure/BitManipulation/totalHammingDistance.cpp
@@ -1,8 +1,8 @@
-// Problem link - (https://leetcode.com/problems/total-hamming-distance/)
+//https://leetcode.com/problems/total-hamming-distance/
 
-class Solution
-{
-public:
+#include <bits/stdc++.h>
+using namespace std;
+
 int totalHammingDistance(vector<int>& nums)
 {
     int ans = 0;
@@ -12,7 +12,7 @@ int totalHammingDistance(vector<int>& nums)
         int ones = 0;
         for(auto ele : nums)
         {
-            if(ele & (1<<bit)
+            if(ele & (1<<bit))
             {
                 ones++;
             }
@@ -25,4 +25,16 @@ int totalHammingDistance(vector<int>& nums)
     }
     return ans;
 }
-};
+
+
+int main()
+{
+    vector<int> nums1 = {4,14,2};
+    cout << totalHammingDistance(nums1) << endl;
+    vector<int> nums2 = {4,14,17,2};
+    cout << totalHammingDistance(nums2) << endl;
+    vector<int> nums3 = {4,14,4};
+    cout << totalHammingDistance(nums3) << endl;
+
+    return 0;
+}
