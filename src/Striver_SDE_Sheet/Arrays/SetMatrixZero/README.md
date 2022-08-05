0a6971b2bbd8d6bcd383c2494b8d310190e5dbb8
Aritra Das <aritrarick2002@gmail.com>
2022-08-05 22:23:42 +0530

    Striver's SDE Sheet Array question added.

---
+++ src/Striver_SDE_Sheet/Arrays/SetMatrixZero/README.md
@@ -0,0 +1,20 @@
+## Problem:
+[Set matrix Zero](https://leetcode.com/problems/set-matrix-zeroes/)
+
+```
+Example:
+
+Input: matrix=[[1,1,1],[1,0,1],[1,1,1]]
+
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Explanation: Since matrix[2][2]=0.Therfore the 2nd column and 2nd row wil be set to 0.
+
+Input: matrix=[[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+
+Output:[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+Explanation:Since matrix[0][0]=0 and matrix[0][3]=0. Therefore 1st row, 1st column and 4th column will be set to 0
+```
+
+[Video Reference](https://www.youtube.com/watch?v=M65xBewcqcI&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=8)
---
+++ src/Striver_SDE_Sheet/Arrays/SetMatrixZero/setmatrixzero.java
@@ -0,0 +1,42 @@
+package Striver_SDE_Sheet.Arrays.SetMatrixZero;
+
+public class setmatrixzero {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 0, 1, 1}, {1, 0, 1, 0}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+
+        System.out.println("Original Matrix: ");
+        printMatrix(arr);
+
+        setZeroes(arr);
+
+        System.out.println("\nModified Matrix: ");
+        printMatrix(arr);
+    }
+
+    private static void printMatrix(int[][] arr) {
+        for (int i=0; i< arr.length; i++) {
+            for (int j=0; j<arr[0].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+    }
+}
