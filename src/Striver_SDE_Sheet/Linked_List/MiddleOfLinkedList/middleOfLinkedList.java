55e4e5bbc1c1c16e85dbe94fabfcac6fb8908641
rishika vishnoi <84177590+Rishikavishnoi@users.noreply.github.com>
2022-08-07 17:17:42 +0530

    Update middleOfLinkedList.java

--- src/Striver_SDE_Sheet/Linked_List/MiddleOfLinkedList/middleOfLinkedList.java
+++ src/Striver_SDE_Sheet/Linked_List/MiddleOfLinkedList/middleOfLinkedList.java
@@ -1,22 +1,74 @@
 package Striver_SDE_Sheet.Linked_List.MiddleOfLinkedList;
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode middleNode(ListNode head) {
-      ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+public class LinkedList{
+ 
+    private Node head;
+ 
+    private static class Node {
+        private int value;
+        private Node next;
+ 
+        Node(int value) {
+            this.value = value;
+ 
         }
-        return slow;
     }
+ 
+    public void addToTheLast(Node node) {
+ 
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+ 
+            temp.next = node;
+        }
+    }
+ 
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.format("%d ", temp.value);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+ 
+    // This function will find middle element in linkedlist
+    // logic implementation
+    public Node findMiddleNode(Node head)
+    {
+        Node slowPointer, fastPointer;
+        slowPointer = fastPointer = head;
+ 
+        while(fastPointer !=null) {
+            fastPointer = fastPointer.next;
+            if(fastPointer != null && fastPointer.next != null) {
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next;
+            }
+        }
+ 
+        return slowPointer;
+ 
+    }
+ 
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        // Creating a linked list
+        Node head=new Node(5);
+        list.addToTheLast(head);
+        list.addToTheLast(new Node(6));
+        list.addToTheLast(new Node(7));
+        list.addToTheLast(new Node(1));
+        list.addToTheLast(new Node(2));
+ 
+        list.printList();
+        // finding middle element
+        Node middle= list.findMiddleNode(head);
+        System.out.println("Middle node will be: "+ middle.value);
+ 
+    }
+ 
 }
