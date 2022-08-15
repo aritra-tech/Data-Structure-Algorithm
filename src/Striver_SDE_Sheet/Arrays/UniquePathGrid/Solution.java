c7b110f9289595b21ea412fbd108303c1e63a7bb
Rishika Vishnoi <Rishikavishnoi56@gmail.com>
2022-08-16 00:37:23 +0530

    readme added

--- src/Striver_SDE_Sheet/Arrays/UniquePathGrid/Solution.java
+++ src/Striver_SDE_Sheet/Arrays/UniquePathGrid/Solution.java
@@ -12,9 +12,14 @@ class Solution{
 	}
 	//solution
 	public static int uniquePaths(int m, int n) {
-        int N=n+m-2;
-        int r=m-1;
+        int N=n+m-2; // how much steps we need to do
+            
+        int r=m-1; // number of steps that need to go dow
         double res=1;
+         // here we calculate the total possible path number 
+            // Combination(N, k) = n! / (k!(n - k)!)
+            // reduce the numerator and denominator and get
+            // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
         
         for(int i=1;i<=r;i++){
             res=res *(N-r+i)/i;
