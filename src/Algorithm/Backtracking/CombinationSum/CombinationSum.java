2c685f479c78b6e4417ab0941b766a6b57015a38
Rakesh Roshan <roshanrakesh7362@gmail.com>
2022-08-21 16:11:13 +0530

    Backtracking Questions added

---
+++ src/Algorithm/Backtracking/CombinationSum/CombinationSum.java
@@ -0,0 +1,27 @@
+package Algorithm.Backtracking.Combination_Sum;
+import java.util.*;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates, 0, target, new ArrayList<>(), result);
+        return result;
+      }
+    
+      private void helper(int[] candidates, int idx, int target, List<Integer> currCombination,
+          List<List<Integer>> result) {
+        if (target == 0) {
+          result.add(new ArrayList<>(currCombination));
+        } 
+        if (target > 0 && idx < candidates.length) {
+          for (int i = idx; i < candidates.length; i++) {
+            currCombination.add(candidates[i]);
+            helper(candidates, i, target - candidates[i], currCombination, result);
+            currCombination.remove(currCombination.size() - 1);
+          }
+        }
+      }
+}
+
+// Time Complexity: O(candidates^target)
+// Space Complexity: O(target)
