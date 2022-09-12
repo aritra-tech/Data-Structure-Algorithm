ad8e3a1eceee68dd78c907162f66a73014b6aee9
Aritra Das <aritrarick2002@gmail.com>
2022-09-13 00:01:51 +0530

    Amazon Questions Added.

---
+++ src/Companywise_Questions/Amazon/Longest_Common_Subsequence/LCS.java
@@ -0,0 +1,39 @@
+package Companywise_Questions.Amazon.Longest_Common_Subsequence;
+
+import java.util.Arrays;
+
+public class LCS {
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+        System.out.println(longestCommonSubsequence(text1, text2));
+    }
+
+    public static int longestCommonSubsequence(String s1, String s2) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+        for (int rows[] : dp)
+            Arrays.fill(rows, -1);
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1))
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                else
+                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+
+        return dp[n][m];
+    }
+}
